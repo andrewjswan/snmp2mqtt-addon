eb5fdd3ba3b7f47ebd207c591d4c567531f8d90b
Andrew J.Swan <ajs.blackhole@gmail.com>
2024-04-11 20:34:14 +0300

    chore: Cosmetic

--- snmp2mqtt/run.sh
+++ snmp2mqtt/run.sh
@@ -1,10 +1,15 @@
-#!/usr/bin/with-contenv bashio
+﻿#!/usr/bin/with-contenv bashio
 
 echo "[$(date)][INFO] SNMP2MQTT start"
 
 CONFIG_PATH=/data/options.json
 
 yq -p json -o yaml /data/options.json > /app/config.yml
+cat /addon_configs/snmp2mqtt/targets.yaml >> /app/config.yml
 
 MQTT_SERVER="$(jq --raw-output '.mqtt.host' $CONFIG_PATH)"
 echo "[$(date)][INFO] Configuration - MQTT Host: $MQTT_SERVER"
+
+cat /app/config.yml
+
+ls /app
