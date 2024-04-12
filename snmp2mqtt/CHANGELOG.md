885da13822e69568f0098ca58c972724213f4afe
Andrew J.Swan <ajs.blackhole@gmail.com>
2024-04-12 19:27:26 +0300

    ## 1.0.0 - 2024-04-13

--- snmp2mqtt/CHANGELOG.md
+++ snmp2mqtt/CHANGELOG.md
@@ -2,6 +2,6 @@
 
 All notable changes to this project will be documented in this file, in reverse chronological order by release.
 
-## 1.0.0 - 2024-04-11
+## 1.0.0 - 2024-04-13
 
 - Initial release.
--- snmp2mqtt/run.sh
+++ snmp2mqtt/run.sh
@@ -1,15 +1,28 @@
-﻿#!/usr/bin/with-contenv bashio
-
-echo "[$(date)][INFO] SNMP2MQTT start"
+#!/usr/bin/with-contenv bashio
 
 CONFIG_PATH=/data/options.json
+TARGET_PATH="$(bashio::config 'targets_path')"
 
-yq -p json -o yaml /data/options.json > /app/config.yml
-cat /addon_configs/snmp2mqtt/targets.yaml >> /app/config.yml
+if [ -z "$TARGET_PATH" ]; then
+    TARGET_PATH="/config/addons_config/snmp2mqtt/targets.yaml"
+fi
 
-MQTT_SERVER="$(jq --raw-output '.mqtt.host' $CONFIG_PATH)"
+if [ ! -f "$TARGET_PATH" ]; then
+    echo "[$(date)][ERROR] Targets not found: $TARGET_PATH"
+    exit 1
+fi
+
+echo "[$(date)][INFO] SNMP2MQTT Start"
+
+echo "[$(date)][INFO] SNMP2MQTT Prepare config file"
+yq -p json -o yaml $CONFIG_PATH > /app/config.main
+grep -v "targets_path" /app/config.main > /app/config.yml
+cat $TARGET_PATH >> /app/config.yml
+
+MQTT_SERVER="$(bashio::config 'mqtt.host')"
+echo "[$(date)][INFO] Configuration - Targets from: $TARGET_PATH"
 echo "[$(date)][INFO] Configuration - MQTT Host: $MQTT_SERVER"
 
-cat /app/config.yml
+node /app/dist/index.js
 
-ls /app
+echo "[$(date)][INFO] SNMP2MQTT Stop"
